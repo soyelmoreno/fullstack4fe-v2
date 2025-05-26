c34b0e3cd7ed3114bf4f7938ba325bdee41f6108
Carlos Moreno <carlos.moreno.4268@gmail.com>
2025-05-26 00:14:10 -0700

    Incorporate chatbot server into main app server

--- app.js
+++ app.js
@@ -1,15 +1,104 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const path = require("path");
+const WebSocketServer = require("ws").Server;
 
+const app = express();
+const PORT = 3000;
+
+// Set up routes and response
 const response = [
   "Hello, world!",
   "This is our NodeJS application server.",
   'Oh, and check out the simple <a href="/chatbot">chatbot app</a>, too.',
 ];
 
+// Home page route
 app.get("/", (req, res) => {
-  res.send(response.join("<br><br>"));
+  res.send(response.map((str) => `<p>${str}</p>`).join(""));
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}`));
+// Serve js files
+app.use("/js", express.static(path.join(__dirname, "chatbot/js/")));
+// Serve css files
+app.use("/css", express.static(path.join(__dirname, "chatbot/css/")));
+
+// Chatbot route
+app.get("/chatbot", (req, res) => {
+  res.sendFile(path.join(__dirname, "chatbot/html/index.html"));
+});
+
+function handleQuery(query, cb) {
+  cb(`You said: ${query}. Response: Awesome`);
+}
+
+// Listen
+const httpServer = app.listen(PORT, () =>
+  console.log(`Example app listening on port ${PORT}`)
+);
+
+// Set up WebSocket
+const wss = new WebSocketServer({ server: httpServer });
+
+/**
+ * Client Counter
+ * Count the number of active connections
+ * @type {Number}
+ */
+let cc = 0;
+wss.on("connection", function connection(ws) {
+  console.log("client connections: ", ++cc);
+
+  ws.on("message", function incoming(message) {
+    try {
+      const { payload, type } = JSON.parse(message);
+      switch (type) {
+        case "query":
+          handleQuery(payload, (response) => {
+            ws.send(
+              JSON.stringify({ type: "queryResponse", payload: response })
+            );
+          });
+          return;
+        default:
+          console.log(message);
+      }
+    } catch (e) {
+      console.error("Error from message: ", e);
+    }
+  });
+
+  // Send welcome message on each connection
+  if (ws.readyState === ws.OPEN) {
+    ws.send(JSON.stringify({ type: "connected", payload: "Welcome!" }));
+  }
+
+  ws.on("close", function close() {
+    --cc;
+    if (cc === 0) {
+      clearInterval(pingInterval);
+    }
+    console.log("disconnected");
+  });
+
+  ws.on("error", function error() {
+    --cc;
+    console.log("error");
+  });
+});
+
+const pingPayload = JSON.stringify({ type: "ping" });
+// Keep the connection alive
+let pingInterval = setInterval(() => {
+  wss.broadcast(pingPayload);
+}, 1 * 5000);
+
+/**
+ * Broadcast data to all connected clients
+ * @param  {Object} data
+ * @void
+ */
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
--- chatbot/js/index.js
+++ chatbot/js/index.js
@@ -1,47 +1,46 @@
-const webSocketOutput = document.querySelector('.output');
-const inputBtn = document.querySelector('.input-box');
+const webSocketOutput = document.querySelector(".output");
+const inputBtn = document.querySelector(".input-box");
 
-inputBtn.addEventListener('keyup', function(event) {
-    if (event.key === "Enter") {
-        const query = event.target.value;
-        event.target.value = '';
-        sendQuery(query);
-    }
+inputBtn.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    const query = event.target.value;
+    event.target.value = "";
+    sendQuery(query);
+  }
 });
 
 let ws;
 try {
   // Local dev
-  if (['localhost', '127.0.0.1', ''].includes(location.hostname)) {
-    ws = new WebSocket(`ws://localhost:3000`);
+  if (["localhost", "127.0.0.1", ""].includes(location.hostname)) {
+    ws = new WebSocket(`ws://localhost:3000/chatbot`);
   } else {
     ws = new WebSocket(`wss://jemisthe.best`);
   }
-} catch(e) {
-  console.log('Web socket init error', e);
+} catch (e) {
+  console.log("Web socket init error", e);
 }
-
 
 function sendQuery(query) {
-    ws.send(JSON.stringify({type: 'query', payload: query}))
+  ws.send(JSON.stringify({ type: "query", payload: query }));
 }
 
-ws.onmessage = function({data}) {
-    const msg = document.createElement('div');
+ws.onmessage = function ({ data }) {
+  const msg = document.createElement("div");
 
-    try {
-        data = JSON.parse(data);
-        const {type, payload} = data;
+  try {
+    data = JSON.parse(data);
+    const { type, payload } = data;
 
-        switch(type) {
-            case 'ping':
-                // console.log('ping');
-                return;
-            default:
-                msg.innerHTML = payload;
-                webSocketOutput.prepend(msg);
-        }
-    } catch(e) {
-        console.log('Websocket error', e);
+    switch (type) {
+      case "ping":
+        // console.log('ping');
+        return;
+      default:
+        msg.innerHTML = payload;
+        webSocketOutput.prepend(msg);
     }
-}
+  } catch (e) {
+    console.log("Websocket error", e);
+  }
+};
