3dcfeb466905f3084410250c055d0faf6a72caf9
Carlos Moreno <carlos.moreno.4268@gmail.com>
2025-05-25 23:17:07 -0700

    Update package.json and main server app

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => {
-  res.send('Hello, world! This is our NodeJS application server');
-});
-app.listen(port, () => console.log(`Example app listening on port ${port}`));
 
+const response = [
+  "Hello, world!",
+  "This is our NodeJS application server.",
+  'Oh, and check out the simple <a href="/chatbot">chatbot app</a>, too.',
+];
+
+app.get("/", (req, res) => {
+  res.send(response.join("<br><br>"));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
